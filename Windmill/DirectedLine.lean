004e1cb1aa5fd7018f0f3efb2a01aeab4b9c9fdc
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2026-08-24 10:02:37 -0600

    refactor: cleanup DirectedLineRep defs

--- Windmill/DirectedLine.lean
+++ Windmill/DirectedLine.lean
@@ -7,65 +7,81 @@ structure Point where
   x : ℝ
   y : ℝ
 
+-- DirectedLineRep
+
 structure DirectedLineRep where
   pt : Point
-  θ : Real.Angle  -- modulo 360 rather than 180, to keep directed-ness
+  θ : Real.Angle
 
 namespace DirectedLineRep
 
+-- Membership
+
 def mem (l : DirectedLineRep) (p : Point) : Prop :=
   ∃ (s : ℝ),
-      (l.pt.x + (s*Real.Angle.cos l.θ) = p.x)
-    ∧ (l.pt.y + (s*Real.Angle.sin l.θ) = p.y)
+      (l.pt.x + s*(Real.Angle.cos l.θ) = p.x)
+    ∧ (l.pt.y + s*(Real.Angle.sin l.θ) = p.y)
 
 instance : Membership Point DirectedLineRep where mem := mem
 
-def IsSameLine (l1 l2 : DirectedLineRep) : Prop :=
-  ∀ (p : Point), p ∈ l1 ↔ p ∈ l2
+-- IsSameLine theorems
 
-end DirectedLineRep
+def IsSameLine (l₁ l₂ : DirectedLineRep) : Prop :=
+  ∀ (p : Point), p ∈ l₁ ↔ p ∈ l₂
+
+theorem sameline_refl : ∀ (x : DirectedLineRep), x.IsSameLine x := by
+  -- unfold DirectedLineRep.IsSameLine
+  intro x p
+  simp
+
+theorem sameline_symm : ∀ {x y : DirectedLineRep}, x.IsSameLine y → y.IsSameLine x := by
+  -- unfold DirectedLineRep.IsSameLine
+  intro x y h p
+  rw [iff_comm_eq]
+  exact h p
+
+theorem sameline_trans : ∀ {x y z : DirectedLineRep},
+  x.IsSameLine y → y.IsSameLine z → x.IsSameLine z := by
+  -- unfold DirectedLineRep.IsSameLine
+  intro x y z hxy hyz p
+  rw [hxy, hyz]
+
+-- Respects theorems
+
+theorem mem_respects (l₁ l₂ : DirectedLineRep)
+  (h : l₁.IsSameLine l₂) :
+  (p ∈ l₁) = (p ∈ l₂) := by
+  -- unfold IsSameLine at h
+  simp only [eq_iff_iff]
+  exact h p
+
 
 instance : Setoid DirectedLineRep where
   r := DirectedLineRep.IsSameLine
   iseqv := by
     constructor
-    · -- refl
-      unfold DirectedLineRep.IsSameLine
-      intro x p
-      simp
-    · -- symm
-      unfold DirectedLineRep.IsSameLine
-      intro x y h p
-      rw [iff_comm_eq]
-      exact h p
-    · -- trans
-      unfold DirectedLineRep.IsSameLine
-      intro x y z hxy hyz p
-      rw [hxy, hyz]
+    · exact sameline_refl
+    · exact sameline_symm
+    · exact sameline_trans
+
+end DirectedLineRep
+
+-- DirectedLine
 
 def DirectedLine := Quotient (inferInstance : Setoid DirectedLineRep)
-
--- Line properties defined on the representation
-
-theorem mem_respects (l₁ l₂ : DirectedLineRep)
-  (h : l₁.IsSameLine l₂) :
-  (p ∈ l₁) = (p ∈ l₂) := by
-  unfold DirectedLineRep.IsSameLine at h
-  simp only [eq_iff_iff]
-  exact h p
 
 namespace DirectedLine
 
 def mem (l : DirectedLine) (p : Point) : Prop :=
   Quotient.lift
-    (fun l => p ∈ l)
-    (fun l₁ l₂ h => mem_respects l₁ l₂ h)
+    (fun l => DirectedLineRep.mem l p)
+    (fun l₁ l₂ h => DirectedLineRep.mem_respects l₁ l₂ h)
     l
 
 instance : Membership Point DirectedLine where mem := mem
 
-end DirectedLine
-
 def colinear (p q r : Point) := ∃ (l : DirectedLine), p∈l ∧ q∈l ∧ r∈l
+
+end DirectedLine
 
 -- #min_imports
