d42c2ae582edba08a34444026f44d51c2eb07aff
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2026-08-31 16:45:06 -0600

    feat: begin improving constructors

--- Windmill/DirectedLine.lean
+++ Windmill/DirectedLine.lean
@@ -23,6 +23,23 @@ instance : Membership Point DirectedLineRep where mem := mem
 
 theorem pt_is_mem (l : DirectedLineRep) : l.pt ∈ l := by use 0; simp
 
+-- constructors
+
+def mk_pt_angle := mk
+
+noncomputable def mk_pt_pt (p q : Point) (h : p ≠ q) : {l : DirectedLineRep // p∈l ∧ q∈l} :=
+  let θ : Real.Angle := p.oangle (q - p)
+  let l : DirectedLineRep := DirectedLineRep.mk p θ
+  have h₁ : p∈l := pt_is_mem l
+  have h₂ : q∈l := by
+    have h := Point.inner_eq_norm_mul_norm_mul_cos_oangle p q
+    change DirectedLineRep.mem l q
+    unfold DirectedLineRep.mem
+    simp [l]
+
+    sorry
+  ⟨l, And.intro h₁ h₂⟩
+
 -- equiv theorems
 
 def equiv (l₁ l₂ : DirectedLineRep) : Prop :=
@@ -76,6 +93,14 @@ def DirectedLine := Quotient (inferInstance : Setoid DirectedLineRep)
 
 namespace DirectedLine
 
+-- constructors
+
+def mk_pt_angle (p : Point) (θ : Real.Angle) : DirectedLine :=
+  Quotient.mk _ (DirectedLineRep.mk_pt_angle p θ)
+
+noncomputable def mk_pt_pt (p q : Point) (h : p ≠ q) : DirectedLine :=
+  Quotient.mk _ (DirectedLineRep.mk_pt_pt p q h)
+
 def mem (l : DirectedLine) (p : Point) : Prop :=
   Quotient.lift
     (fun l => DirectedLineRep.mem l p)
@@ -88,7 +113,8 @@ def mem (l : DirectedLine) (p : Point) : Prop :=
 
 instance : Membership Point DirectedLine where mem := mem
 
-def colinear (p q r : Point) := ∃ (l : DirectedLine), p∈l ∧ q∈l ∧ r∈l
+def colinear (p q r : Point) :=
+  (p ≠ q ∧ q ≠ r ∧ r ≠ p) ∧ ∃ (l : DirectedLine), p∈l ∧ q∈l ∧ r∈l
 
 end DirectedLine
 
